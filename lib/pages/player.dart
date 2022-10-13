f25f05f38a25384a21c7d9f661a304fe6dbd7cff
J-Bungus <113871762+J-Bungus@users.noreply.github.com>
2022-10-12 21:14:25 -0400

    Add files via upload

--- lib/pages/player.dart
+++ lib/pages/player.dart
@@ -8,7 +8,6 @@ class Player extends StatefulWidget {
 }
 
 class _PlayerState extends State<Player> {
-
   bool playerOne = true;
   List<String> playerOneMoves = [];
   List<String> playerTwoMoves = [];
@@ -19,17 +18,64 @@ class _PlayerState extends State<Player> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.orange[900],
         title: const Text("Player"),
       ),
       body: Container(
-        child: Column(
-          children: [
-            const Text("Player"),
-            const Text("Turn: "),
-            TextField(),
-            TextButton(
-                onPressed: () {},
-                child: const Text("Go!")
+        color: Colors.orange[200],
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children:[
+            Column(
+              children:[
+                Padding(
+                  padding: const EdgeInsets.only(top: 20.0),
+                  child: const Text(
+                    "Player",
+                    style: TextStyle(
+                      fontSize: 35.0,
+                    ),
+                  ),
+                ),
+
+                const Text(
+                  "Turn: ",
+                  style: TextStyle(
+                    fontSize: 35.0,
+                  ),
+                ),
+
+                SizedBox(
+                  width: 240,
+                  child: TextField(),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: ElevatedButton(
+                      onPressed: () {},
+                      child: const Text(
+                        "Go!",
+                        style: TextStyle(
+                          fontSize: 24.0
+                        ),
+                      ),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.amber[900],
+                      side: BorderSide(color: Colors.deepOrange, width: 4),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 80.0),
+                  child: Text(
+                    "Moves Played:",
+                    style: TextStyle(
+                      fontSize: 25.0,
+                    ),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
--- lib/pages/rules.dart
+++ lib/pages/rules.dart
@@ -12,10 +12,66 @@ class _RulesState extends State<Rules> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.orange[900],
         title: const Text("Rules"),
       ),
       body: Container(
-        child: const Text("Rules"),
+        color: Colors.orange[200],
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
+                  child: Text(
+                      "Rules",
+                    style: TextStyle(
+                      fontSize: 50.0,
+                      fontWeight: FontWeight.bold,
+                      decoration: TextDecoration.underline,
+                      decorationThickness: 2.4,
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(top: 7.0, left: 14.0, right: 7.0, bottom: 7.0),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      width: 4,
+                        color: Colors.orange
+                    ),
+                    borderRadius: BorderRadius.circular(10),
+
+                  ),
+                  child: const SizedBox(
+                    width: 300,
+                    child: Text(
+                        "[INSERT ACTUAL NAME] is made as a simple test for geography knowledge.\n\n"
+                            "1. Type in the name of a present-day geographical location. "
+                            "It can be a country, capital city, and continent.\n\n"
+                            "2. Then pass the phone to your friend for them to enter another location.\n\n"
+                            "3. The first letter of the location must be the same as the last letter of the "
+                            "location entered previously. "
+                            "If you are playing with the computer, the location will be automatically generated.\n\n"
+                            "5. Location names cannot be repeated.\n\n"
+                            "6. Alternate entering names until either player can no longer think of any more location names.\n\n"
+                            "7. To win, the other player must be enter a location name that follows all the previous rules.\n\n"
+                            "8. If the other player also cannot think of a location name, then the game ends in a tie.",
+                    textDirection: TextDirection.ltr,
+                    textAlign: TextAlign.left,
+                      style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
